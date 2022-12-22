3e68e9adfe7d83ae7d21555bbffe388020517b6e
Brian <Brian@SEALION>
2022-12-22 12:31:47 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Fotothing
+{
+    partial class FotothingHarvester
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GoButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PwdTxt = new System.Windows.Forms.TextBox();
+            this.TargetFolder = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StatusBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(68, 296);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(184, 46);
+            this.GoButton.TabIndex = 0;
+            this.GoButton.Text = "Grab Photos";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // UserTxt
+            // 
+            this.UserTxt.Location = new System.Drawing.Point(204, 65);
+            this.UserTxt.Name = "UserTxt";
+            this.UserTxt.Size = new System.Drawing.Size(200, 39);
+            this.UserTxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // PwdTxt
+            // 
+            this.PwdTxt.Location = new System.Drawing.Point(204, 131);
+            this.PwdTxt.Name = "PwdTxt";
+            this.PwdTxt.Size = new System.Drawing.Size(200, 39);
+            this.PwdTxt.TabIndex = 4;
+            this.PwdTxt.UseSystemPasswordChar = true;
+            // 
+            // TargetFolder
+            // 
+            this.TargetFolder.Location = new System.Drawing.Point(204, 202);
+            this.TargetFolder.Name = "TargetFolder";
+            this.TargetFolder.ReadOnly = true;
+            this.TargetFolder.Size = new System.Drawing.Size(445, 39);
+            this.TargetFolder.TabIndex = 6;
+            this.TargetFolder.TabStop = false;
+            this.TargetFolder.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TargetFolder_MouseDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Folder";
+            // 
+            // StatusBox
+            // 
+            this.StatusBox.Location = new System.Drawing.Point(258, 278);
+            this.StatusBox.Multiline = true;
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.StatusBox.Size = new System.Drawing.Size(749, 284);
+            this.StatusBox.TabIndex = 7;
+            this.StatusBox.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(258, 558);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(749, 22);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // FotothingHarvester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 634);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.StatusBox);
+            this.Controls.Add(this.TargetFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PwdTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GoButton);
+            this.Name = "FotothingHarvester";
+            this.Text = "Harvest Your Fotothing Photos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button GoButton;
+        private Label label1;
+        private TextBox UserTxt;
+        private Label label2;
+        private TextBox PwdTxt;
+        private TextBox TargetFolder;
+        private Label label3;
+        private TextBox StatusBox;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using HtmlAgilityPack;
+
+namespace Fotothing
+{
+    public partial class FotothingHarvester : Form
+    {
+        //System.Collections.Generic.IEnumerable<string> AuthCookies;
+        CookieContainer cookieContainer = new CookieContainer();
+        List<string> linkList = new List<string>();
+        string BaseDir = "";
+        int completed = 0;
+        public FotothingHarvester()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        /*
+        private void HarvestImages(List<string> links)
+        {
+            List<string> TryAgainList = new List<string>();
+            string BaseDir = @"C:\temp\Fotothing\";
+            var handler = new HttpClientHandler
+            {
+                UseCookies = true
+            };
+            handler.CookieContainer = cookieContainer;
+
+            using (HttpClient client = new HttpClient(handler))
+                foreach (var link in links)
+                {
+
+                    var imagename = link.Substring(link.LastIndexOf("/") + 1);
+                    var fileName = BaseDir + imagename;
+                    Task<HttpResponseMessage> responseTask = client.GetAsync(link);
+                    responseTask.Wait();
+                    HttpResponseMessage response = responseTask.Result;
+                    Task<Stream> streamTask = response.Content.ReadAsStreamAsync();
+                    streamTask.Wait();
+                    Stream stream = streamTask.Result;
+                    bool fail = false;
+                    using (stream)
+                    using (FileStream fileStream = File.Open(fileName, FileMode.Create))
+                    {
+                        stream.CopyTo(fileStream);
+                        if (fileStream.Length < 300)
+                        {
+                            fail = true;
+                        }
+                    }
+                    if (fail)
+                    {
+                        TryAgainList.Add(link);
+                        File.Delete(fileName);
+                    }
+                    
+                }
+            if (TryAgainList.Count > 0)
+                HarvestImages(TryAgainList);
+
+        }
+        */
+        private async void HarvestImages(List<string> links)
+        {
+            List<string> TryAgainList = new List<string>();
+            //string BaseDir = @"C:\temp\Fotothing\";
+            var handler = new HttpClientHandler
+            {
+                UseCookies = true
+            };
+            handler.CookieContainer = cookieContainer;
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                //ParallelOptions options = new ParallelOptions
+                //{
+                //    MaxDegreeOfParallelism = 2
+                //};
+                
+                foreach (var link in links)
+                {
+                    
+                    var imagename = link.Substring(link.LastIndexOf("/") + 1);
+                    var fileName = BaseDir + imagename;
+                    if (!File.Exists(fileName))
+                    {
+                        try
+                        {
+                            HttpResponseMessage response = await client.GetAsync(link);
+                            Stream stream = await response.Content.ReadAsStreamAsync();
+                            bool fail = false;
+                            using (stream)
+                            using (FileStream fileStream = File.Open(fileName, FileMode.Create))
+                            {
+                                await stream.CopyToAsync(fileStream);
+                                if (fileStream.Length < 300)
+                                {
+                                    fail = true;
+                                }
+                                else
+                                {
+                                    completed++;
+                                }
+                            }
+                            if (fail)
+                            {
+                                lock (TryAgainList)
+                                {
+                                    TryAgainList.Add(link);
+                                    File.Delete(fileName);
+                                }
+
+                            }
+                        }
+                        catch
+                        {
+                            lock (TryAgainList)
+                            {
+                                File.Delete(fileName);
+                                TryAgainList.Add(link);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        completed++;
+                    }
+                    progressBar1.Value = (int)(((decimal)completed / (decimal)linkList.Count) * 100);
+                    if (completed % 100 == 0 )
+                    {
+                        SetStatus(completed.ToString() + " photos downloaded - " + (((double)completed / linkList.Count)).ToString("P"));
+                    }
+                }
+            }
+            if (TryAgainList.Count > 0)
+                HarvestImages(TryAgainList);
+        }
+
+        private void GetLinks()
+        {
+            int page = 0;
+            while (true)
+            {
+                List<string> pagelinkList = new List<string>();
+                page++;
+
+                var handler = new HttpClientHandler
+                {
+                    UseCookies = true
+                };
+                handler.CookieContainer = cookieContainer;
+                var client = new HttpClient(handler);
+                string target = "http://www.fotothing.com/listing.php?page=" + page.ToString();
+                var getresult = client.GetAsync(target).Result;
+                if (getresult.IsSuccessStatusCode)
+                {
+                    var html = getresult.Content.ReadAsStringAsync().Result;
+                    var doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(html);
+                    var links = doc.DocumentNode.SelectNodes("//a");
+
+                    // Iterate through the list of anchor tags and retrieve the "href" attribute for each tag
+
+                    foreach (var link in links)
+                    {
+                        pagelinkList.Add(link.Attributes["href"].Value);
+                    }
+
+                }
+                foreach (var link in pagelinkList)
+                {
+                    if (link.StartsWith("http"))
+                    {
+                        linkList.Add(link);
+                    }
+                }
+                if (page >1 && pagelinkList[1].ToLower().StartsWith("http") && !(pagelinkList[0].ToLower().StartsWith("http")))
+                {
+                    break;
+                }
+            }
+        }
+
+        private bool AuthtoFotothing()
+        {
+            // Create a new HttpClient object to send the POST request
+            var client = new HttpClient();
+
+            // Create a new HttpContent object with the data to send in the POST request
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("email", UserTxt.Text.Trim()),
+                new KeyValuePair<string, string>("pass", PwdTxt.Text.Trim()),
+            });
+
+            // Send the POST request
+            var response = client.PostAsync("http://www.fotothing.com/login.php", content).Result;
+
+            // Check the response status code to ensure that the POST request was successful
+            if (response.IsSuccessStatusCode)
+            {
+                // Store the authentication cookies returned by the server
+                var cookies = response.Headers.GetValues("Set-Cookie");
+                foreach (var cookie in cookies)
+                {
+                    cookieContainer.SetCookies(new Uri("http://www.fotothing.com"), cookie);
+                }
+
+                return true;
+                //MessageBox.Show("Authentication successful. Cookies: " + string.Join(", ", cookies));
+            }
+            else
+            {
+                MessageBox.Show("Authentication failed with status code: " + response.StatusCode);
+                return false;
+            }
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            if (UserTxt.Text.Trim().Length > 0 && PwdTxt.Text.Trim().Length > 0 && Directory.Exists(TargetFolder.Text))
+                if (AuthtoFotothing())
+                {
+                    SetStatus("Authenticated");
+                    GetLinks();
+
+                    if (linkList.Count() > 0)
+                    {
+                        SetStatus("There are " + linkList.Count + " photos to harvest");
+                        HarvestImages(linkList);
+                    }
+                    else
+                        SetStatus("Unable to find any photos to harvest");
+                }
+        }
+
+        private void SetStatus(string v)
+        {
+           var now= DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            StatusBox.Text += now + " - " + v+Environment.NewLine;
+            StatusBox.Select(StatusBox.Text.Length, 0);
+            StatusBox.ScrollToCaret();
+        }
+
+        private void TargetFolder_MouseDown(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog
+            {
+                InitialDirectory = @"C:\"
+            };
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                BaseDir = folderBrowserDialog.SelectedPath + @"\";
+            }
+            TargetFolder.Text = BaseDir;
+
+        }
+    }
+}
